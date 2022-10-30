085becf37646270425926dcd44c545b0451c1603
Valdrin Muja <valdrin.muja@smart-bits.ch>
2022-10-30 09:16:55 +0100

    Create and implement Basket controller

--- src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -3,7 +3,6 @@
     public class ShoppingCart
     {
         public string UserName { get; set; }
-
         public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
         public ShoppingCart()
         {
